7696bf83bf6cfee22c2f853c3cebedf3a5dbf7a8
Patryk Czarnik <p.czarnik@alx.pl>
2025-12-17 10:23:35 +0100

    Pierwsza wersja kalkulatora

---
+++ src/main/java/pl/alx/kalkulator/LogikaKalkulatora.java
@@ -0,0 +1,18 @@
+package pl.alx.kalkulator;
+
+public class LogikaKalkulatora {
+
+    public String[] dostepneOperacje() {
+        return new String[] { "+", "-", "*" };
+    }
+
+    public long oblicz(long liczba1, long liczba2, String operacja) {
+        return switch(operacja) {
+            case "+" -> liczba1 + liczba2;
+            case "-" -> liczba1 - liczba2;
+            case "*" -> liczba1 * liczba2;
+            default -> throw new IllegalArgumentException("Nieznana operacja " + operacja);
+        };
+    }
+
+}
---
+++ src/test/java/pl/alx/kalkulator/LogikaKalkulatoraTest.java
@@ -0,0 +1,34 @@
+package pl.alx.kalkulator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LogikaKalkulatoraTest {
+    private LogikaKalkulatora logika;
+
+    @BeforeEach
+    void setUp() {
+        logika = new LogikaKalkulatora();
+    }
+
+    @Test
+    void testPlus() {
+        var result = logika.oblicz(12, 13, "+");
+        assertEquals(25, result);
+    }
+
+    @Test
+    void testMinus() {
+        var result = logika.oblicz(20, 7, "-");
+        assertEquals(13, result);
+    }
+
+    @Test
+    void testMnoz() {
+        var result = logika.oblicz(3, 7, "*");
+        assertEquals(21, result);
+    }
+
+}
